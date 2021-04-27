8ed1661e9ad4816a5d6a34a29a04d9cf48992a86
fellianik <tanakanaomi04@gmail.com>
2021-04-28 01:46:02 +0700

    menambah class setiap section

--- js/script.js
+++ js/script.js
@@ -19,28 +19,28 @@ $(window).scroll(function () {
   });
 
   //   about
-  if (wScroll > $("#about").offset().top - 200) {
-    $("#about .profile").each(function (i) {
+  if (wScroll > $(".about").offset().top - 200) {
+    $(".about .profile").each(function (i) {
       setTimeout(function () {
-        $("#about .profile").eq(i).addClass("aMuncul");
+        $(".about .profile").eq(i).addClass("aMuncul");
       }, 275 * (i + 1));
     });
   }
 
   //   skill
-  if (wScroll > $("#skill").offset().top - 250) {
-    $("#skill .gambar").each(function (i) {
+  if (wScroll > $(".skill").offset().top - 250) {
+    $(".skill .gambar").each(function (i) {
       setTimeout(function () {
-        $("#skill .gambar").eq(i).addClass("sMuncul");
+        $(".skill .gambar").eq(i).addClass("sMuncul");
       }, 275 * (i + 1));
     });
   }
 
   // project
-  if (wScroll > $("#project").offset().top - 250) {
-    $("#project .card").each(function (i) {
+  if (wScroll > $(".project").offset().top - 250) {
+    $(".project .card").each(function (i) {
       setTimeout(function () {
-        $("#project .card").eq(i).addClass("pMuncul");
+        $(".project .card").eq(i).addClass("pMuncul");
       }, 300 * (i + 1));
     });
   }
